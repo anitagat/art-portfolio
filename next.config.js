f338b1b3c02d72e842345b06763957aac1255955
Anita Gattei <anita.gattei@gmail.com>
2025-07-06 22:43:35 +0200

    add github pipeline config

---
+++ next.config.js
@@ -0,0 +1,8 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',
+  basePath: '/art-portfolio',
+  assetPrefix: '/art-portfolio/',
+};
+
+module.exports = nextConfig;
