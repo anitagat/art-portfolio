0a1b65c27d98f9b290ee168ebc5626fa4875178e
Anita Gattei <anita.gattei@gmail.com>
2025-07-06 23:24:08 +0200

    image fix - static website

--- next.config.js
+++ next.config.js
@@ -3,6 +3,9 @@ const nextConfig = {
   output: 'export',
   basePath: '/art-portfolio',
   assetPrefix: '/art-portfolio/',
+  images: {
+    unoptimized: true,
+  },
 };
 
 module.exports = nextConfig;
